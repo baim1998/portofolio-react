87bdffdf8ce02907fb60948e6fbe25a9e8f2be2a
baim1998 <najad16081998@gmail.com>
2024-07-01 11:27:36 +0700

    remove state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+
 import './App.css'
 import Home from './component/home'
 import './component/home.css'
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
