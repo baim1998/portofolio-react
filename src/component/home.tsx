a24f84985684ac93cc38d0331e49ead60da909e0
baim1998 <najad16081998@gmail.com>
2024-07-01 10:55:24 +0700

    init

---
+++ src/component/home.tsx
@@ -0,0 +1,165 @@
+function Home() {
+  return (
+    <div>
+    <div id="navbar">
+       <div id="logo-nav">
+      <img id="logo" src="src/assets/logo/smart_toy_24dp_FILL0_wght400_GRAD0_opsz24.png" />
+      baim
+      </div>
+       <div id="menu-item">
+        <ul id="item">
+          <li>Home</li>
+          <li>Service</li>
+          <li>Portofolio</li>
+        </ul>
+       </div>
+       <div id="button-nav">
+        <button id="buttonsend">
+            <p style={{fontWeight:'1000',fontFamily:'Franklin Gothic Medium,Arial Narrow,Arial, sans-serif',fontSize:'12px',marginTop:'5px'}}>
+            Let's Talk
+              </p>
+             <img style={{marginTop:'5px'}} src="src/assets/logo/send.png" width="15px" />
+          </button>
+       </div>
+  </div>
+  <div id="intro-nav">
+  <div id="about-nav">
+    <h4 id="hello"> Hello Welcome</h4>
+    <h1 id="name">I'am Baim <br />
+    Belajar Coding Fullstack
+    <div id="icon-sosmed">
+     <h5 id="attention">Follow me</h5> 
+    <img id="discord" src="src/assets/logo/discord.png"/>
+    <img id="github" src="src/assets/logo/github.png"/>
+    <img id="tiktok" src="src/assets/logo/tiktok.png"/>
+    <img id="instagram" src="src/assets/logo/instagram.png"/>
+    </div>
+  </h1>
+  </div>
+  <div id="profile-nav">
+    <img id="foto" src="src/assets/logo/luffy.jpg" />
+  </div>
+  </div>
+  <div id="text-navbar">
+    <div>
+    <h2 id="header2">What can i do</h2>
+    </div>
+    <div id="aboutme-nav">
+     <p id="paragraf">I am a beginner who has entered the world of frontend developer,<br /> 
+      you can see from my very simple portfolio, <br />
+     but I am still trying to make it better in the future. </p>
+    </div> 
+  </div>
+    <div id="box">
+    <div id="iconweb">
+        <img id="logoweb" src="src/assets/logo/web.png"/>
+        <p id="textweb">Web Design</p>
+      </div>
+      <div id="paragraf-web">
+        <p id="paragrafweb1">Website design using a modern and trending <br /> design in the market</p>
+      </div>
+      <div id="next-logo">
+        <img id="nextlogo" src="src/assets/logo/next.png"/>
+      </div>
+      
+      
+    </div>
+    <div id="box1">
+    <div id="iconweb1">
+        <img id="logoweb1" src="src/assets/logo/app.png"/>
+        <p id="textweb1">Apps Design</p>
+      </div>
+      <div id="paragraf-web1">
+        <p id="paragrafweb11">App design while maintaining user comfort <br /> when using the application</p>
+      </div>
+      <div id="next-logo1">
+        <img id="nextlogo1" src="src/assets/logo/next.png"/>
+      </div>
+      
+      
+    </div>
+    <div id="box2">
+    <div id="iconweb2">
+        <img id="logoweb2" src="src/assets/logo/front.png"/>
+        <p id="textweb2">Frontend <br /> Developer</p>
+      </div>
+      <div id="paragraf-web2">
+        <p id="paragrafweb12">Frontend web developer by implementing ui/ux <br />design into code  </p>
+      </div>
+      <div id="next-logo2">
+        <img id="nextlogo2" src="src/assets/logo/next.png"/>
+      </div>
+      
+      
+    </div>
+    <div id="box3">
+    <div id="iconweb3">
+        <img id="logoweb3" src="src/assets/logo/backend.png"/>
+        <p id="textweb3">Backend <br />Developer</p>
+      </div>
+      <div id="paragraf-web3">
+        <p id="paragrafweb13">Backend web developer by creating logic that will be used by the user</p>
+      </div>
+      <div id="next-logo3">
+        <img id="nextlogo3" src="src/assets/logo/next.png"/>
+      </div>
+      
+      
+    </div>
+    <div id="porto-nav">
+     <div id="porto1">
+      <img id="design1" src="src/assets/logo/react1.jpg"  />
+     </div>
+     <div id="porto2">
+      <img id="design2" src="src/assets/logo/react2.jpg"  />
+     </div>
+    </div>
+    <div id="porto-nav1">
+    
+      <div id="portoa">
+        <img id="designa" src="src/assets/logo/react3.jpg"  />
+      </div>
+      <div id="portob">
+        <img id="designb" src="src/assets/logo/react4.jpg"/>
+      </div>
+    </div>
+   <div id="baground">
+    <h1 style={{fontFamily:'roboto,sans-serif',fontWeight:'600',fontSize:'25px', textAlign:"center",paddingTop:'20px'}}>About me</h1>
+    
+    <div>
+      <p style={{textAlign:'center',paddingTop:'20px',fontSize:'20px',fontFamily:'Roboto,sans-serif',fontWeight:'600'}}>BAIM <p style={{fontSize:'15px',fontWeight:'200',marginBottom:'10px'}}>Belajar Coding Fullstack</p>
+        <img style={{borderRadius:'20px'}} src="src/assets/logo/luffy2.jpg" width="200px" /></p>
+      <p style={{fontSize:'15px',fontFamily:'sans-serif',marginTop:'10px',textAlign:'center'}}>I graduated From SMK NURUL MUKMIN with Computer and Network Engineering study Program, <br />
+        I have experience in Frontend Developer.
+      </p>
+    </div>
+   
+   </div>
+    <div id="nav-end">
+      <div id="caprtion-icon">
+        <h3 style={{fontFamily:'Roboto,sans-serif',marginTop :'10px',color:'white'}}>Want to make <br />beautiful products ?</h3>
+        <button style={{display:'flex',justifyContent:'space-around',width:'100px',height:'30px',marginTop:'10px',borderRadius:'20px',marginBottom:'20px',backgroundColor:'#BB8FCE ',}}>
+          <p style={{paddingTop:'7px',fontFamily:'roboto,sans-serif',fontWeight:'bold',color:'white'}}>Hire me</p>
+          <img style={{paddingTop:'3px',paddingRight:'3px'}} src="src/assets/logo/message.png" width={'20px'} />
+          </button>
+      </div>
+      <div id="logo-text">
+        <p style={{marginTop:'10px',color:'white'}}>help me to be more enthusiastic in developing <br />
+          other ideas
+        </p>
+        <br />
+        <div style={{display:'flex',justifyContent:'left', gap:'10px'}}>
+        <p style={{color:'white',marginTop:'7px'}}>follow me :</p>       
+        <img style={{paddingLeft:'10px',}} src="src/assets/logo/discord1.png"width='30px'/>
+        <img  src="src/assets/logo/github1.png" width='30px' />
+        <img  src="src/assets/logo/tik-tok1.png"width='30px'  />
+        <img  src="src/assets/logo/instagram1.png"width='30px'  /></div>
+      </div>
+    </div>
+   
+   </div>
+   
+  )
+}
+
+export default Home
